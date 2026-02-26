24e5a45ab429bc95ecbd80c8fb2ea5cf0e9ec10d
Josiah-Rigg <josiah.rigg@stu.ocsb.ca>
2026-02-26 13:16:20 -0500

    Changed comments dates

--- main.ts
+++ main.ts
@@ -1,8 +1,21 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Josiah
+ * Created on: Feb 2026
+ * This program Tell the Temperature.
 */
 
-basic.showString('Hello, World!')
+let temperature: number
+
+//clear screen
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// Temperature 
+input.onButtonPressed(Button.A, function () {
+    temperature = input.temperature()
+    basic.showString("The temperature is: " + temperature + " C")
+    pause(1000)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
